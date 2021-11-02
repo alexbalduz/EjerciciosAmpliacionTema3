950e9f8f68b772b292a05214e4073deed9d49aa4
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-11-02 16:34:22 +0100

    errores corregidos

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -12,10 +12,10 @@ import sys
 #
 def aVeryBigSum(ar):
     # Write your code here
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    ar_count = int(input().strip())
-    ar = list(map(int, input().rstrip().split()))
-    result = aVeryBigSum(ar)
-    fptr.write(str(result) + '\n')
-    fptr.close()
+    if __name__ == '__main__':
+        fptr = open(os.environ['OUTPUT_PATH'], 'w')
+        ar_count = int(input().strip())
+        ar = list(map(int, input().rstrip().split()))
+        result = aVeryBigSum(ar)
+        fptr.write(str(result) + '\n')
+        fptr.close()
