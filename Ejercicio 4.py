5cd6d63cb81199246bf3571d2fc1c1c279ecdc97
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-11-03 09:28:42 +0100

    cambios

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -11,10 +11,8 @@ import sys
 #
 def staircase(n):
     # Write your code here
-    for fila in range(n):
-        for columna in range(fila + 1):
-            print("#", end = "")
-        print()
+    for i in range(n):
+        print("#"*(i+1))
 
     if __name__ == '__main__':
         n = int(input().strip())
