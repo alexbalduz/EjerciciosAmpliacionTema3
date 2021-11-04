62a17c44ac71fa786bfbd49b9f494a2b36664a51
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-11-04 15:15:17 +0100

    cambios

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -17,16 +17,17 @@ import sys
 #
 def countApplesAndOranges(s, t, a, b, apples, oranges):
     # Write your code here
-    if __name__ == '__main__':
-        first_multiple_input = input().rstrip().split()
-        s = int(first_multiple_input[0])
-        t = int(first_multiple_input[1])
-        second_multiple_input = input().rstrip().split()
-        a = int(second_multiple_input[0])
-        b = int(second_multiple_input[1])
-        third_multiple_input = input().rstrip().split()
-        m = int(third_multiple_input[0])
-        n = int(third_multiple_input[1])
-        apples = list(map(int, input().rstrip().split()))
-        oranges = list(map(int, input().rstrip().split()))
-        countApplesAndOranges(s, t, a, b, apples, oranges)
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+    s = int(first_multiple_input[0])
+    t = int(first_multiple_input[1])
+    second_multiple_input = input().rstrip().split()
+    a = int(second_multiple_input[0])
+    b = int(second_multiple_input[1])
+    third_multiple_input = input().rstrip().split()
+    m = int(third_multiple_input[0])
+    n = int(third_multiple_input[1])
+    apples = list(map(int, input().rstrip().split()))
+    oranges = list(map(int, input().rstrip().split()))
+    countApplesAndOranges(s, t, a, b, apples, oranges)
