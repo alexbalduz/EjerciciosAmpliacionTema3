6576cbfd99fb448e928f27d3a01f1df6a0ec3685
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-11-07 18:57:16 +0100

    terminado

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -17,17 +17,17 @@ import sys
 #
 def countApplesAndOranges(s, t, a, b, apples, oranges):
     # Write your code here
-    count_Apples= 0
-    count_Oranges= 0
+    counter_Apples= 0
+    counter_Oranges= 0
 
     for i in range(len(apples)):
         if a+apples[i] >= s and a+apples[i] <= t:
-            count_Apples +=1
+            counter_Apples +=1
     for i in range(len(oranges)):
         if b+oranges[i] >= s and b+oranges[i] <=t:
-            count_Oranges +=1
-    print(count_Apples)
-    print(count_Oranges)
+            counter_Oranges +=1
+    print(counter_Apples)
+    print(counter_Oranges)
 
 if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
